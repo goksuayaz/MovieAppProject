dbd1a4c97e21d26f0bd992ae356074477f0061a4
Göksu Ayaz <goksuayaz79@gmail.com>
2024-11-14 17:13:07 +0300

    trendingMovies

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
+
+let config = getDefaultConfig(__dirname);
+
+config = withNativeWind(config, { input: "./global.css" });
+config = wrapWithReanimatedMetroConfig(config);
+
+module.exports = config;
